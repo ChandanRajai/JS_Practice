8763b3595903dea757564e51ba146568a0cce1be
ChandanRajai <140834639+ChandanRajai@users.noreply.github.com>
2023-09-29 12:03:52 +0000

    IIFE(immediately invoked function expression)

--- Day_01/question_01.js
+++ Day_01/question_01.js
@@ -1,1 +1,15 @@
-console.log("Practice for commit");
+(function(){
+    var a=b=3;
+})();
+
+console.log(typeof a);
+console.log(typeof b);
+
+/*Explanation
+ 
+function(){
+    b=3    ____________________b becomes as global scope
+    var a=b____________________a becomes as local varaiable of function
+}
+ so b as a number and a cant access the outside of function so it has undefined
+ */
